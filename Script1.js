f6f1f2cfe044f3a3fee624adc02eb0f793f94fc1
indra <indraramadhan55@gmail.com>
2020-05-02 14:54:01 +0800

    tes
    tess

---
+++ Script1.js
@@ -0,0 +1,16 @@
+  
+$(function () {
+
+    let text = "#PahlawanGardaDepann";
+
+    let i = 0;
+    setInterval(() => {
+        $("#hashtag").append(text[i]);
+        i++;
+        if (i === 20) {
+            $("#hashtag").html("");
+            i = 0;
+        }
+    }, 200);
+
+});// JavaScript source code
